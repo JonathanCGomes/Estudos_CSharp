882deeda26c1f056e77808cb1858356c0872c285
Jonathan Cesar <jonathan10ccg@gmail.com>
2025-06-18 18:08:53 -0300

    Estrutura de pastas e primeiro exercicio Calc_Retangulo

---
+++ Projeto_Classes/Calc_Retangulo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+
+Retangulo r = new Retangulo();
+
+Console.Write("Digite a largura e altura de um retangulo: ");
+r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine($"Área: {r.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"Perímetro: {r.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"Diagonal: {r.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
