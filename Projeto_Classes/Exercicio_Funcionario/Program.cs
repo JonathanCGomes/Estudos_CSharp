2170624ae682e6b9d6c9bc4137efe92e1115dfcf
Jonathan Cesar <jonathan10ccg@gmail.com>
2025-06-19 14:49:02 -0300

    Exercicio basico: Classe Funcionario

---
+++ Projeto_Classes/Exercicio_Funcionario/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercicio_Funcionario;
+using System;
+using System.Globalization;
+
+Funcionario func = new Funcionario();
+
+Console.Write("Nome:");
+func.Nome = Console.ReadLine();
+Console.Write("Salario Bruto:");
+func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Imposto:");
+func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Funcionário: " + func);
+
+Console.Write("Digite a porcentagem para aumentar o salario: ");
+double porcentAumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+func.AumentarSalario(porcentAumento);
+
+Console.WriteLine("Dados atualizados: " + func);
