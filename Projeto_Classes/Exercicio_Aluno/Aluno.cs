28b096dbf3d659919bbc03cdda73468ce2376b8a
Jonathan Cesar <jonathan10ccg@gmail.com>
2025-06-27 19:04:20 -0300

    Exercicio Classes aprovacao Aluno

---
+++ Projeto_Classes/Exercicio_Aluno/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Aluno
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double NotaA, NotaB, NotaC;
+
+        public void SituacaoAluno()
+        {
+            double resultado = NotaA + NotaB + NotaC;
+            double notaFaltante = 60.00 - resultado;
+
+            Console.WriteLine($"Nota Final: {resultado:F2}");
+
+            if (resultado >= 60.00)
+            {
+                Console.WriteLine($"Aprovado");
+            }
+            else
+            {
+                Console.WriteLine($"Reprovado");
+                Console.WriteLine($"Faltaram {notaFaltante:F2} pontos");
+            }
+        }
+    }
+}
---
+++ Projeto_Classes/Exercicio_Aluno/Program.cs
@@ -0,0 +1,18 @@
+﻿using Exercicio_Aluno;
+using System.Xml;
+using System.Globalization;
+
+Aluno aluno = new Aluno();
+
+Console.Write("Nome do aluno:");
+aluno.Nome = Console.ReadLine();
+
+
+Console.WriteLine("Digite as tres notas do aluno:");
+aluno.NotaA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+aluno.NotaB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+aluno.NotaC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+aluno.SituacaoAluno();
+
